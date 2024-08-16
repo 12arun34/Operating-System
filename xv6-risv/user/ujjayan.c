763d6ae1826dd74115c7c841abbc14163dcb3fb6
12arun34 <111728536+12arun34@users.noreply.github.com>
2024-08-17 01:59:24 +0530

    Add files via upload

---
+++ xv6-risv/user/ujjayan.c
@@ -0,0 +1,11 @@
+#include<kernel/types.h>
+#include<user/user.h>
+int main()
+{
+   
+   printf("ujjayan willl appear .Don't worry just wait and watch\n");
+   //printf("%d\n",getpid());
+   //int x = arun();
+   printf("%s\n",arun());
+   exit(0);
+}
---
+++ xv6-risv/user/ujjayan.d
@@ -0,0 +1,1 @@
+user/ujjayan.o: user/ujjayan.c kernel/types.h user/user.h
